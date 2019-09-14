97ba86f2ca673abaedab04a6154a6a295a8a1afa
Amanda Kuenzle <amandakunzle@gmail.com>
2019-09-14 22:38:13 +0200

    added the pride flag, changed the colors

--- transbadge.ino
+++ transbadge.ino
@@ -30,20 +30,17 @@ void setup() {
   strip.setBrightness(10); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
-
 // loop() function -- runs repeatedly as long as board is on ---------------
 void loop() {
 
-  transColors();
+  transFlag();
   strip.show();
-  delay(500);
-}
+  delay(5000);
 
-// Transgender Pride Flag Colors Hex, RGB & CMYK Codes
-// Color	Information
-// Maya Blue	Name: Maya Blue Hex: #55CDFC RGB: (85, 205, 252) CMYK: 0.662, 0.186, 0, 0.011
-// White	Name: White Hex: #FFFFFF RGB: (255, 255, 255) CMYK: 0, 0, 0, 0
-// Amaranth Pink	Name: Amaranth Pink Hex: #F7A8B8 RGB: (247, 168, 184) CMYK: 0, 0.319, 0.255, 0.031
+  prideFlag();
+  strip.show();
+  delay(5000);
+}
 
 // Feather NeoPixel matrix adresses
 //  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7
@@ -51,7 +48,7 @@ void loop() {
 // 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23
 // 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31
 
-// Arrays to define the rows
+// Arrays to define the rows, since flags are mostly row based
 int row1[4] = {0, 8, 16, 24};
 int row2[4] = {1, 9, 17, 25};
 int row3[4] = {2, 10, 18, 26};
@@ -61,16 +58,44 @@ int row6[4] = {5, 13, 21, 29};
 int row7[4] = {6, 14, 22, 30};
 int row8[4] = {7, 15, 23, 31};
 
-void transColors() {
+// Color declaration
+// Transflag
+uint32_t mayaBlue = strip.Color(0, 128, 255);
+uint32_t amaranthPink = strip.Color(255, 0, 127);
+uint32_t white = strip.Color(255, 255, 255);
+// prideFlag
+uint32_t darkViolet = strip.Color(75, 0, 130);
+uint32_t blue = strip.Color(0, 0, 255);
+uint32_t lightGreen = strip.Color(0, 255, 0);
+uint32_t yellow = strip.Color(255, 255, 0);
+uint32_t orange = strip.Color(255, 128, 0);
+uint32_t red = strip.Color(204, 0, 0);
+uint32_t off = strip.Color(0, 0, 0);
+
+void transFlag() {
   for (int i = 0; i < 4; i++)
   {
-    strip.setPixelColor((row1[i]), 0, 128, 255); // Maya Blue
-    strip.setPixelColor((row2[i]), 255, 0, 127); // Amaranth Pink
-    strip.setPixelColor((row3[i]), 255, 0, 127); // Amaranth Pink
-    strip.setPixelColor((row4[i]), 255, 255, 255); // White
-    strip.setPixelColor((row5[i]), 255, 255, 255); // White
-    strip.setPixelColor((row6[i]), 255, 0, 127); // Amaranth Pink
-    strip.setPixelColor((row7[i]), 255, 0, 127); // Amaranth Pink
-    strip.setPixelColor((row8[i]), 0, 128, 255); // Maya Blue
+    strip.setPixelColor((row1[i]), mayaBlue);
+    strip.setPixelColor((row2[i]), amaranthPink);
+    strip.setPixelColor((row3[i]), amaranthPink);
+    strip.setPixelColor((row4[i]), white);
+    strip.setPixelColor((row5[i]), white);
+    strip.setPixelColor((row6[i]), amaranthPink);
+    strip.setPixelColor((row7[i]), amaranthPink);
+    strip.setPixelColor((row8[i]), mayaBlue);
   }  
+}
+
+void prideFlag() {
+  for (int i = 0; i < 4; i++)
+  {
+    strip.setPixelColor((row1[i]), off);
+    strip.setPixelColor((row2[i]), amaranthPink);
+    strip.setPixelColor((row3[i]), blue);
+    strip.setPixelColor((row4[i]), lightGreen);
+    strip.setPixelColor((row5[i]), yellow);
+    strip.setPixelColor((row6[i]), orange);
+    strip.setPixelColor((row7[i]), red);
+    strip.setPixelColor((row8[i]), off);
+  }
 }
